225d3ca9a022f359e31d6b170d8eafa3f6f14242
Dipesh <45828484+dipeshb1@users.noreply.github.com>
2024-10-07 14:15:26 +0530

    Minor fixes

--- src/Components/FeatureBox.jsx
+++ src/Components/FeatureBox.jsx
@@ -4,7 +4,7 @@ function FeatureBox({ icon, heading, para, cta }) {
             <div className="border rounded-xl p-2">
                 <img src={icon} alt="feature-icon" />
             </div>
-            <h3 className="font-semibold text-xl py-4">{heading}</h3>
+            <h3 className="font-semibold text-xl py-4 text-center">{heading}</h3>
             <p className="text-center text-para font-light">{para}</p>
             {cta && <a href="#features" className="text-primary my-5">Learn more →</a>}
         </div>
